65732c84126304cb6c7a58e892e65580a455ec75
PushkarGaur10 <115537133+PushkarGaur10@users.noreply.github.com>
2022-10-18 13:17:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+for(i=0;i<7;i++)
+document.querySelectorAll(".btn")[i].addEventListener("click",function(){
+  var clickbtn = this.innerHTML;
+  
+  switching(clickbtn);
+})  
+document.addEventListener("keypress",function(event){
+ switching(event.key);
+})
+
+
+
+function switching(key){
+  switch(key){
+      case "a" : var crash = new Audio("sounds/crash.mp3");
+                 crash.play();
+      break;
+    
+      case "s" : var kickbass = new Audio("sounds/kick-bass.mp3");
+                 kickbass.play();
+      break;
+
+      case "d" : var snare = new Audio("sounds/snare.mp3");
+                 snare.play();
+      break;
+
+      case "w" : var tom1 = new Audio("sounds/tom-1.mp3");
+                 tom1.play();
+      break;
+
+      case "q" : var tom2 = new Audio("sounds/tom-2.mp3");
+                 tom2.play();
+      break;
+
+      case "e" : var tom3 = new Audio("sounds/tom-3.mp3");
+                 tom3.play();
+      break;
+
+      case "x" : var tom4 = new Audio("sounds/tom-4.mp3");
+                 tom4.play();
+      break;
+    }
+}
